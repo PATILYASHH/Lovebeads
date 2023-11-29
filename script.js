f69f2af9f2da75b0570b1b6aec82afe7c5a367b7
PATILYASHH <149749700+PATILYASHH@users.noreply.github.com>
2023-11-29 16:00:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+product
+function hoverProduct(element) {
+    element.style.transform = 'scale(1.1)';
+}
+
+// Function to handle unhover effect on product
+function unhoverProduct(element) {
+    element.style.transform = 'scale(1)';
+}
