70064fc75417a6557e8ead6abb6ba026946289ad
PATILYASHH <149749700+PATILYASHH@users.noreply.github.com>
2023-11-30 04:00:45 +0000

    Love beads 1.0.0

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function hoverProduct(element) {
     element.style.transform = 'scale(1.1)';
 }
 
-// Function to handle unhover effect on product
 function unhoverProduct(element) {
     element.style.transform = 'scale(1)';
 }
